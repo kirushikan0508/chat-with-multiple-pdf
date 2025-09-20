8fb2a8d36a66d39a6961008c5fefbb9ac7143606
kirushikan <kirushikankiru01@gmail.com>
2025-09-20 11:20:21 +0530

    read the pdf and extract the text

--- app.py
+++ app.py
@@ -1,6 +1,25 @@
 import streamlit as st
-import pyPDF2 import pdfReade
+from pyPDF2 import pdfReader
 from langchain_text_splitters import RecursiveCharacterTextSplitter
 import os
 
-from
+from langchain_google_genai import GoogleGenerativeAIEmbeddings
+import google.generativeai as genai
+from langchain.vectorstores import FAISS
+from langchain_google_genai import ChatGoogleGenerativeAI
+from langchain.chains.question_answering import load_qa_chain
+from langchain.prompts import PromptTemplate
+from dotenv import load_dotenv
+
+load_dotenv()
+
+genai.configure(api_key=os.getenv("GOOGLE_API_KEY"))
+
+#extract the pdf and get the pdf details
+def get_pdf_text(pdf_docs):
+    text=""
+    for pdf in pdf_docs:
+        pdf_reader=pdfReader(pdf)
+        for page in pdf_reader.pages:
+            text+=page.extract_text()
+    return text
